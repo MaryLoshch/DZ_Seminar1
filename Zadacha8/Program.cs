09503fc03e8ad25a82d7224a0dbba8fb18ee1caa
мария <oops.mari@mail.ru>
2022-11-27 23:06:17 +0300

    Задача 8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+while (x <= Num)
+{
+    if (x % 2 == 0)
+    {
+        Console.Write($"{x} " );
+        x++;
+
+    }
+    else
+    {
+        x++;
+    }
+}
