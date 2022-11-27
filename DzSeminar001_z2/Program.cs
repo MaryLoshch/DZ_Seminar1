24878cb822770eb2cd8f50eb022f708ec300083d
мария <oops.mari@mail.ru>
2022-11-27 20:57:31 +0300

    Задача 2

--- DzSeminar001_z2/Program.cs
+++ DzSeminar001_z2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое целое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+
+if(numA > numB)
+{
+    max = numA;
+    min = numB;
+}
+else
+{
+    max = numB;
+    min = numA;
+}
+Console.WriteLine("Большее из представленных чисел " + max);
+Console.WriteLine("Меньшее из представленных чисел " + min);
