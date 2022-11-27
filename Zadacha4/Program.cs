d604963d0f4852bfb3f13a1956e5f1a6f132e6d3
мария <oops.mari@mail.ru>
2022-11-27 21:45:25 +0300

    Задача 4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите первое целое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число ");
+int numC = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(numA > numB)
+{
+    max = numA;
+    if(numA > numC)
+    {
+        max = numA;
+    }
+    else
+    {
+        max = numC;
+    }
+}
+else
+{
+max = numB;
+if(numB > numC)
+    {
+        max = numB;
+    }
+    else
+    {
+        max = numC;
+    }
+}
+Console.WriteLine($"Большее из представленных чисел {max}");
